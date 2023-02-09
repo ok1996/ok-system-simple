ecd4c5a4257d5d1ea8b8d55472d859d313990096
ok1996 <ku29@qq.com>
2023-02-09 15:16:55 +0800

    optimize: simple-starter-datasource模块依赖修改

--- simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/domain/PageRequest.java
+++ simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/domain/PageRequest.java
@@ -1,25 +1,25 @@
 package cn.iosd.starter.datasource.domain;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * 分页请求参数
  *
  * @author ok1996
  */
 public class PageRequest<T> {
-    /**
-     * 起始页
-     */
+
+    @Schema(description = "起始页")
     private Integer pageNum;
-    /**
-     * 每页显示记录数
-     */
+
+    @Schema(description = "每页显示记录数")
     private Integer pageSize;
-    /**
-     * 排序
-     */
+
+    @Schema(description = "排序")
     private String orderBy;
 
+    @Schema(description = "查询条件参数")
     private T data;
 
     public Integer getPageNum() {
--- simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/domain/PageResponse.java
+++ simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/domain/PageResponse.java
@@ -1,6 +1,7 @@
 package cn.iosd.starter.datasource.domain;
 
 import com.github.pagehelper.PageInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.List;
 
@@ -10,24 +11,16 @@ import java.util.List;
  * @author ok1996
  */
 public class PageResponse<T> {
-    /**
-     * 当前页
-     */
+    @Schema(description = "当前页")
     private int pageNum;
 
-    /**
-     * 每页显示记录数
-     */
+    @Schema(description = "每页显示记录数")
     private int pageSize;
 
-    /**
-     * 记录总数
-     */
+    @Schema(description = "记录总数")
     private long total;
 
-    /**
-     * 数据列表
-     */
+    @Schema(description = "数据列表")
     private List<T> list;
 
     public static <T> PageResponse<T> getPage(List<T> list) {
