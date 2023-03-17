52fd32ae1fe3c6b6dd75cb3d2041208ea4b94dfc
ok1996 <ku29@qq.com>
2023-03-17 17:11:04 +0800

    optimize: [simple-starter-redisson]双重检查锁改为静态内部类的单例模式：线程安全

---
+++ simple-starter/simple-starter-redisson/src/main/java/cn/iosd/starter/redisson/service/RedissonConfigFactory.java
@@ -0,0 +1,37 @@
+package cn.iosd.starter.redisson.service;
+
+import cn.iosd.starter.redisson.properties.RedissonProperties;
+import cn.iosd.starter.redisson.service.impl.ClusterConfigImpl;
+import cn.iosd.starter.redisson.service.impl.MasterSlaveConfigImpl;
+import cn.iosd.starter.redisson.service.impl.SentinelConfigImpl;
+import cn.iosd.starter.redisson.service.impl.StandaloneConfigImpl;
+import org.redisson.config.Config;
+
+/**
+ * @author ok1996
+ */
+public class RedissonConfigFactory {
+    /**
+     * 静态内部类-单例模式
+     */
+    private static class RedissonConfigFactoryHolder {
+        private static final RedissonConfigFactory INSTANCE = new RedissonConfigFactory();
+    }
+
+    public static RedissonConfigFactory getInstance() {
+        return RedissonConfigFactoryHolder.INSTANCE;
+    }
+
+    public Config createConfig(RedissonProperties redissonProperties) {
+        String connectionType = redissonProperties.getType();
+        RedissonConfigService redissonConfigService = switch (connectionType) {
+            case "standalone" -> new StandaloneConfigImpl();
+            case "sentinel" -> new SentinelConfigImpl();
+            case "cluster" -> new ClusterConfigImpl();
+            case "masterSlave" -> new MasterSlaveConfigImpl();
+            default -> throw new IllegalArgumentException("创建Redisson连接Config失败！当前连接方式:" + connectionType);
+        };
+        return redissonConfigService.createRedissonConfig(redissonProperties);
+    }
+}
+
--- simple-starter/simple-starter-redisson/src/main/java/cn/iosd/starter/redisson/service/RedissonManager.java
+++ simple-starter/simple-starter-redisson/src/main/java/cn/iosd/starter/redisson/service/RedissonManager.java
@@ -1,10 +1,6 @@
 package cn.iosd.starter.redisson.service;
 
 import cn.iosd.starter.redisson.properties.RedissonProperties;
-import cn.iosd.starter.redisson.service.impl.ClusterConfigImpl;
-import cn.iosd.starter.redisson.service.impl.MasterSlaveConfigImpl;
-import cn.iosd.starter.redisson.service.impl.SentinelConfigImpl;
-import cn.iosd.starter.redisson.service.impl.StandaloneConfigImpl;
 import org.redisson.Redisson;
 import org.redisson.config.Config;
 
@@ -30,52 +26,6 @@ public class RedissonManager {
 
     public Redisson getRedisson() {
         return redisson;
-    }
-
-    /**
-     * Redisson连接方式配置工厂
-     * 双重检查锁
-     */
-    static class RedissonConfigFactory {
-
-        private RedissonConfigFactory() {
-        }
-
-        private static volatile RedissonConfigFactory factory = null;
-
-        public static RedissonConfigFactory getInstance() {
-            if (factory == null) {
-                synchronized (Object.class) {
-                    if (factory == null) {
-                        factory = new RedissonConfigFactory();
-                    }
-                }
-            }
-            return factory;
-        }
-
-
-        /**
-         * 根据连接类型获取对应连接方式的配置,基于策略模式
-         *
-         * @param redissonProperties redis连接信息
-         * @return Config
-         */
-        Config createConfig(RedissonProperties redissonProperties) {
-
-            String connectionType = redissonProperties.getType();
-            //声明配置上下文
-            RedissonConfigService redissonConfigService;
-            switch (connectionType) {
-                case "standalone" -> redissonConfigService = new StandaloneConfigImpl();
-                case "sentinel" -> redissonConfigService = new SentinelConfigImpl();
-                case "cluster" -> redissonConfigService = new ClusterConfigImpl();
-                case "masterSlave" -> redissonConfigService = new MasterSlaveConfigImpl();
-                default ->
-                        throw new IllegalArgumentException("创建Redisson连接Config失败！当前连接方式:" + connectionType);
-            }
-            return redissonConfigService.createRedissonConfig(redissonProperties);
-        }
     }
 
 }
