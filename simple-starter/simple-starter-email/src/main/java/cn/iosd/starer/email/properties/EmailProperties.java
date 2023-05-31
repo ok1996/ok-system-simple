1c2ad909332d5118918d93fa81685ed6413b91f9
ok1996 <ku29@qq.com>
2023-05-31 09:30:27 +0800

    optimize:[simple-starter-email] 重构服务：简化方法、增加自定义发件人邮件配置参数方法

--- simple-starter/simple-starter-email/src/main/java/cn/iosd/starer/email/properties/EmailProperties.java
+++ simple-starter/simple-starter-email/src/main/java/cn/iosd/starer/email/properties/EmailProperties.java
@@ -1,5 +1,6 @@
 package cn.iosd.starer.email.properties;
 
+import cn.iosd.starer.email.vo.EmailConfigVo;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,82 +10,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties("simple.email")
 public class EmailProperties {
-    /**
-     * SMTP服务器host
-     */
-    private String smtpHost;
-    /**
-     * SMTP服务器端口号
-     */
-    private Integer smtpPort;
-    /**
-     * 发件人邮箱用户名
-     */
-    private String username;
-    /**
-     * 发件人邮箱密码
-     */
-    private String password;
-    /**
-     * 发件人邮箱地址
-     */
-    private String fromEmail;
-    private Boolean smtpAuthEnable = true;
-    private Boolean smtpStarttlsEnable = true;
 
-    public String getSmtpHost() {
-        return smtpHost;
+    private EmailConfigVo config;
+
+    public EmailConfigVo getConfig() {
+        return config;
     }
 
-    public void setSmtpHost(String smtpHost) {
-        this.smtpHost = smtpHost;
-    }
-
-    public Integer getSmtpPort() {
-        return smtpPort;
-    }
-
-    public void setSmtpPort(Integer smtpPort) {
-        this.smtpPort = smtpPort;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFromEmail() {
-        return fromEmail;
-    }
-
-    public void setFromEmail(String fromEmail) {
-        this.fromEmail = fromEmail;
-    }
-
-    public Boolean getSmtpAuthEnable() {
-        return smtpAuthEnable;
-    }
-
-    public void setSmtpAuthEnable(Boolean smtpAuthEnable) {
-        this.smtpAuthEnable = smtpAuthEnable;
-    }
-
-    public Boolean getSmtpStarttlsEnable() {
-        return smtpStarttlsEnable;
-    }
-
-    public void setSmtpStarttlsEnable(Boolean smtpStarttlsEnable) {
-        this.smtpStarttlsEnable = smtpStarttlsEnable;
+    public void setConfig(EmailConfigVo config) {
+        this.config = config;
     }
 }
