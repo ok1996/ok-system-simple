ba66923d3f60404d61dd9f3002944b0410ba749a
ok1996 <ku29@qq.com>
2023-06-02 09:46:11 +0800

    optimize: update readme.md and schema description

--- simple-demo/simple-demo-email/src/main/java/cn/iosd/demo/email/vo/SendEmailVo.java
+++ simple-demo/simple-demo-email/src/main/java/cn/iosd/demo/email/vo/SendEmailVo.java
@@ -11,20 +11,27 @@ import java.util.Map;
  */
 @Data
 public class SendEmailVo {
-    @Schema(description = "收件人的电子邮件地址", example = "testxxx@qq.com,testxxx@foxmail.com")
+    @Schema(description = "收件人的邮件地址列表", example = "xxx@qq.com,xxx@foxmail.com")
     private List<String> toEmails;
-    @Schema(description = "抄送人的电子邮件地址列表", example = "testyyy@qq.com,testyyy@foxmail.com")
+
+    @Schema(description = "抄送人的邮件地址列表", example = "yyy@qq.com,yyy@foxmail.com")
     private List<String> ccEmails;
+
     @Schema(description = "附件文件的路径列表", example = "E:\\temp\\新建文本文档.txt")
     private List<String> attachments;
+
     @Schema(description = "邮件主题")
     private String subject;
+
     @Schema(description = "邮件内容")
     private String content;
+
     @Schema(description = "指示邮件内容是否为HTML格式", defaultValue = "false")
     private Boolean isHtml;
-    @Schema(description = "密送人的电子邮件地址列表", example = "testzzz@qq.com")
+
+    @Schema(description = "密送人的邮件地址列表", example = "zzz@qq.com")
     private List<String> bccEmails;
+
     @Schema(description = "内联图片的路径和CID映射", hidden = true)
     Map<String, String> inlineImages;
 }
