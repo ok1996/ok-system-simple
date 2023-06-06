e0a48fa2613e97d66fb72676abe05aa25593ecad
ok1996 <ku29@qq.com>
2023-06-06 11:03:51 +0800

    optimize:[demo-base-param]优化示例逻辑

--- simple-demo/demo-base-param/src/main/java/cn/iosd/demo/base/param/service/TestService.java
+++ simple-demo/demo-base-param/src/main/java/cn/iosd/demo/base/param/service/TestService.java
@@ -1,20 +1,21 @@
 package cn.iosd.demo.base.param.service;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import cn.iosd.base.param.service.IBaseParamService;
 import cn.iosd.base.param.utils.ParamInitUtil;
 import cn.iosd.base.param.vo.BaseParamCodeValueVo;
 import cn.iosd.demo.base.param.init.TestParamInit;
 import cn.iosd.demo.base.param.vo.ClassmateVo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author ok1996
  */
+@Slf4j
 @Service
 public class TestService {
     @Autowired(required = false)
@@ -22,24 +23,28 @@ public class TestService {
 
     /**
      * 获取同学列表信息
+     *
      * @return 返回同学列表信息的ClassmateVo对象
      */
     public ClassmateVo classmateList() {
-        if (baseParamService != null) {
-            // 从基础参数服务中获取基础参数码值对象的列表
-            List<BaseParamCodeValueVo<?>> simulation = baseParamService.selectCodeValueVoParamByKey(TestParamInit.KEY);
-            // 判断是否开启模拟数据
-            if (ParamInitUtil.getBooleanValueByCodeDefaultFalse(simulation, TestParamInit.OPEN_SIMULATION_CODE)) {
-                // 获取存储的模拟数据，并将其转换为对应的实体类
-                Optional<BaseParamCodeValueVo<?>> contentData = ParamInitUtil.getBaseParamCodeValueVoByCode(simulation, TestParamInit.CONTENT_DATA_CODE);
-                if (contentData.isPresent()) {
-                    return ParamInitUtil.readValue(contentData.get(), ClassmateVo.class);
-                }
-            } else {
-                // TODO: 实现获取其他来源途径的数据的代码
-            }
+        if (baseParamService == null) {
+            return null;
         }
-        return null;
+        // 从基础参数服务中获取基础参数码值对象的列表
+        List<BaseParamCodeValueVo<?>> simulation = baseParamService.selectCodeValueVoParamByKey(TestParamInit.KEY);
+        // 判断是否开启模拟数据
+        if (!ParamInitUtil.getBooleanValueByCodeDefaultFalse(simulation, TestParamInit.OPEN_SIMULATION_CODE)) {
+            // TODO: 实现获取其他来源途径的数据的代码
+            log.info("获取其他来源途径的数据");
+            return null;
+        }
+        // 获取存储的模拟数据，并将其转换为对应的实体类
+        Optional<BaseParamCodeValueVo<?>> contentData = ParamInitUtil.getBaseParamCodeValueVoByCode(simulation, TestParamInit.CONTENT_DATA_CODE);
+        if (!contentData.isPresent()) {
+            log.error("模拟数据不存在", TestParamInit.KEY);
+            return null;
+        }
+        return ParamInitUtil.readValue(contentData.get(), ClassmateVo.class);
     }
 
 }
