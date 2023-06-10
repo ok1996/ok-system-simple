5815a62f63f113bcda34b4ed7c1143e3fdf361f5
ok1996 <ku29@qq.com>
2023-06-10 15:03:28 +0800

    optimize:[simple-base-param]优化配置文件结构

--- simple-base/simple-base-param/src/main/java/cn/iosd/base/param/config/BaseParamAutoConfiguration.java
+++ simple-base/simple-base-param/src/main/java/cn/iosd/base/param/config/BaseParamAutoConfiguration.java
@@ -1,16 +1,24 @@
 package cn.iosd.base.param.config;
 
 import org.flywaydb.core.Flyway;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
 /**
+ * 配置MapperScan、Flyway、ComponentScan以及是否启用
+ *
  * @author ok1996
  */
 @Configuration
-public class ParamFlywayConfig {
+@ComponentScan(value = {"cn.iosd.base.param"})
+@MapperScan(basePackages = "cn.iosd.base.param.mapper")
+@ConditionalOnProperty(value = "simple.base.param.enabled", havingValue = "true", matchIfMissing = true)
+public class BaseParamAutoConfiguration {
 
     @Bean
     public Flyway paramFlyway(DataSource dataSource) {
