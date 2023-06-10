b9414f493781a64acd4d7bf2c3d3029b90a9c937
ok1996 <ku29@qq.com>
2023-06-10 15:03:28 +0800

    optimize:[simple-base-param]优化配置文件结构

--- simple-demo/simple-demo-datasource/src/main/java/cn/iosd/demo/datasource/config/DemoDatasourceAutoConfiguration.java
+++ simple-demo/simple-demo-datasource/src/main/java/cn/iosd/demo/datasource/config/DemoDatasourceAutoConfiguration.java
@@ -1,12 +1,36 @@
 package cn.iosd.demo.datasource.config;
 
+import org.flywaydb.core.Flyway;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 /**
+ * 配置MapperScan、Flyway
+ *
  * @author ok1996
  */
 @Configuration
 @MapperScan("cn.iosd.demo.datasource.mapper")
 public class DemoDatasourceAutoConfiguration {
+
+    /**
+     * 配置Flyway迁移脚本信息
+     *
+     * @param dataSource 数据源
+     * @return Flyway对象
+     */
+    @Bean
+    public Flyway datasourceFlyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/mysql/ds")
+                .baselineOnMigrate(true)
+                .table("datasource_flyway_schema_history")
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
 }
