a28145c235367fce27fcbb32ac04167a05e8b840
ok1996 <ku29@qq.com>
2023-06-12 10:31:46 +0800

    version: 2022.3.5.0

--- simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/database/MybatisGenerator.java
+++ simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/database/MybatisGenerator.java
@@ -19,7 +19,7 @@ public class MybatisGenerator {
     /**
      * 依赖模块版本号
      */
-    private static final String SIMPLE_VERSION = "2022.3.5.0-SNAPSHOT";
+    private static final String SIMPLE_VERSION = "2022.3.5.0";
 
     /**
      * 文件输出地址前缀
