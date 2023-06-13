bc15a6216d3969f5feeea1d21f53949901df4000
ok1996 <ku29@qq.com>
2023-06-13 10:13:43 +0800

    version: 2023.3.5.1-SNAPSHOT

--- simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/database/MybatisGenerator.java
+++ simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/database/MybatisGenerator.java
@@ -19,7 +19,7 @@ public class MybatisGenerator {
     /**
      * 依赖模块版本号
      */
-    private static final String SIMPLE_VERSION = "2023.3.6.0-SNAPSHOT";
+    private static final String SIMPLE_VERSION = "2023.3.5.1-SNAPSHOT";
 
     /**
      * 文件输出地址前缀
