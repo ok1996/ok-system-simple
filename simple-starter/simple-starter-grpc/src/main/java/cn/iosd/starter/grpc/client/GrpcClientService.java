6e2adc18e0b5b4d5b790040f72c54d8788d074fb
ok1996 <ku29@qq.com>
2023-06-29 10:32:27 +0800

    optimize:[simple-starter-grpc]独立获取超时时间、获取请求头方法

--- simple-starter/simple-starter-grpc/src/main/java/cn/iosd/starter/grpc/client/GrpcClientService.java
+++ simple-starter/simple-starter-grpc/src/main/java/cn/iosd/starter/grpc/client/GrpcClientService.java
@@ -35,12 +35,18 @@ public class GrpcClientService implements InitializingBean {
     @Autowired
     private GrpcClientProperties grpcClientProperties;
 
+    /**
+     * gRPC客户端Bean注入初始化类
+     */
     @Autowired
     private InitializeGrpcClientBeans beanInjection;
 
     @Autowired
     private ApplicationContext applicationContext;
 
+    /**
+     * 客户端调用开始时的请求头列表
+     */
     @Autowired(required = false)
     private List<ClientCallStartHeaders> clientCallStartHeadersList;
 
@@ -78,22 +84,8 @@ public class GrpcClientService implements InitializingBean {
             Class<?> type = grpcClientBean.field().getType();
             Field field = grpcClientBean.field();
             Object bean = grpcClientBean.bean();
-
-            long timeout;
-            if (-1 != annotation.timeout()) {
-                timeout = annotation.timeout();
-            } else {
-                timeout = grpcClientProperties.getTimeout();
-            }
-            ClientCallStartHeaders headers = null;
-            if (!CollectionUtils.isEmpty(clientCallStartHeadersList)) {
-                if (clientCallStartHeadersList.size() == 1 && StringUtils.isBlank(annotation.headerBeanName())) {
-                    headers = clientCallStartHeadersList.get(0);
-                } else {
-                    String beanName = annotation.headerBeanName();
-                    headers = StringUtils.isNotBlank(beanName) ? clientCallStartHeadersMap.get(beanName) : null;
-                }
-            }
+            long timeout = getTimeout(annotation);
+            ClientCallStartHeaders headers = getHeaders(annotation);
 
             GrpcChannelProperties properties = grpcClientProperties.getChannel().get(annotationValue);
             ManagedChannel client = GrpcChannel.getChannel(properties.getAddress(), timeout, headers);
@@ -111,5 +103,44 @@ public class GrpcClientService implements InitializingBean {
                 field.setAccessible(accessible);
             }
         }
+    }
+
+    /**
+     * 获取超时时间
+     * 如果在注解中指定了超时时间，则使用注解中定义的超时时间；
+     * 否则，使用配置文件中的默认超时时间。
+     *
+     * @param annotation 调用方传入的注解对象
+     * @return 超时时间（单位：毫秒）
+     */
+    private long getTimeout(GrpcClient annotation) {
+        if (annotation.timeout() != -1) {
+            return annotation.timeout();
+        } else {
+            return grpcClientProperties.getTimeout();
+        }
+    }
+
+    /**
+     * 获取请求头
+     * 如果请求头列表不为空，根据以下条件进行判断：
+     * <p/>
+     * - 当请求头列表只有一个元素且未指定 headerBeanName 时，直接使用该元素作为请求头；
+     * <p/>
+     * - 否则，根据指定的 headerBeanName 从 clientCallStartHeadersMap 中获取对应的请求头。
+     *
+     * @param annotation 调用方传入的注解对象
+     * @return 请求头对象，如果无法获取则返回 null
+     */
+    private ClientCallStartHeaders getHeaders(GrpcClient annotation) {
+        if (!CollectionUtils.isEmpty(clientCallStartHeadersList)) {
+            if (clientCallStartHeadersList.size() == 1 && StringUtils.isBlank(annotation.headerBeanName())) {
+                return clientCallStartHeadersList.get(0);
+            } else {
+                String beanName = annotation.headerBeanName();
+                return StringUtils.isNotBlank(beanName) ? clientCallStartHeadersMap.get(beanName) : null;
+            }
+        }
+        return null;
     }
 }
