426e5d97c0ed4eaebfd72f9f4dbee43ff92ebbb9
ok1996 <ku29@qq.com>
2023-07-07 10:18:48 +0800

    feat:[simple-starter-gateway]增加微服务网关基础模块及示例

---
+++ simple-demo/simple-demo-gateway/src/main/java/cn/iosd/demo/gateway/DemoGatewayApplication.java
@@ -0,0 +1,16 @@
+package cn.iosd.demo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ok1996
+ */
+@SpringBootApplication
+public class DemoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGatewayApplication.class, args);
+    }
+
+}
