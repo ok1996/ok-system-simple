94f987bdd15053f02a8db903700ce3eb5772eb89
ok1996 <ku29@qq.com>
2023-07-19 10:24:57 +0800

    optimize:[simple-demo-socket] ParameterObject-适配JDK8

--- simple-demo/simple-demo-socket-two/src/main/java/cn/iosd/demo/socket/two/controller/MessageController.java
+++ simple-demo/simple-demo-socket-two/src/main/java/cn/iosd/demo/socket/two/controller/MessageController.java
@@ -1,6 +1,6 @@
 package cn.iosd.demo.socket.two.controller;
 
-import org.springdoc.core.annotations.ParameterObject;
+import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
