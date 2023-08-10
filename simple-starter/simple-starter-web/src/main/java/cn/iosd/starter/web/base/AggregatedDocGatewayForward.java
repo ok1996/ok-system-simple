ae9dd03279786916e6896d450bfc03d917ffd81f
ok1996 <ku29@qq.com>
2023-08-10 16:20:46 +0800

    optimize:[simple-starter-web]避免重定向文档地址时的异常问题 (问题描述)[https://github.com/springdoc/springdoc-openapi/issues/2341]

--- simple-starter/simple-starter-web/src/main/java/cn/iosd/starter/web/base/AggregatedDocGatewayForward.java
+++ simple-starter/simple-starter-web/src/main/java/cn/iosd/starter/web/base/AggregatedDocGatewayForward.java
@@ -1,8 +1,13 @@
 package cn.iosd.starter.web.base;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.mvc.ParameterizableViewController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 聚合网关文档所需转发视图
@@ -11,8 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
  *
  * @author ok1996
  */
-@Controller
+@Configuration
 public class AggregatedDocGatewayForward {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     /**
      * 当网关层配置过滤器为：将请求的路径前缀去除，例如配置内容如下
@@ -23,14 +31,22 @@ public class AggregatedDocGatewayForward {
      * <br/>
      * 则所需聚合文档增加转发包含应用名称的接口地址
      */
-    @GetMapping("/${spring.application.name}/v3/api-docs/{param}")
-    public String forwardV3ApiDocsParam(@PathVariable String param) {
-        return "forward:/v3/api-docs/" + param;
+    @Bean
+    public SimpleUrlHandlerMapping customUrlHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(1);
+        Map<String, Object> urlMap = new HashMap<>(4);
+        urlMap.put("/" + applicationName + "/v3/api-docs/{param}", parameterizableViewController("v3/api-docs/{param}"));
+        urlMap.put("/" + applicationName + "/v3/api-docs", parameterizableViewController("v3/api-docs"));
+        urlMap.put("/" + applicationName + "/v3/api-docs/default", parameterizableViewController("v3/api-docs"));
+        mapping.setUrlMap(urlMap);
+        return mapping;
     }
 
-    @GetMapping(value = {"/${spring.application.name}/v3/api-docs"
-            , "/${spring.application.name}/v3/api-docs/default"})
-    public String forwardV3ApiDocs() {
-        return "forward:/v3/api-docs";
+    private ParameterizableViewController parameterizableViewController(String viewName) {
+        ParameterizableViewController controller = new ParameterizableViewController();
+        controller.setViewName("forward:/" + viewName);
+        return controller;
     }
 }
+
