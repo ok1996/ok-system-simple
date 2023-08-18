487ddca687c5f6b366d3e84ac96517dc3a27114e
ok1996 <ku29@qq.com>
2023-08-18 17:05:53 +0800

    optimize:[simple-base-generator]优化simple-base-generator结构

--- simple-demo/demo-base-generator/src/main/java/cn/iosd/demo/base/generator/BaseGeneratorApplication.java
+++ simple-demo/demo-base-generator/src/main/java/cn/iosd/demo/base/generator/BaseGeneratorApplication.java
@@ -1,35 +1,16 @@
 package cn.iosd.demo.base.generator;
 
 
-import cn.iosd.base.generator.api.database.service.DatabaseGenService;
-import cn.iosd.base.generator.api.database.vo.DatabaseGenVo;
-import lombok.extern.slf4j.Slf4j;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author ok1996
  */
-@Slf4j
+@SpringBootApplication
 public class BaseGeneratorApplication {
     public static void main(String[] args) {
-        DatabaseGenVo vo = new DatabaseGenVo();
-        vo.setAuthorName("ok1996");
-        vo.setDataBaseUrl("jdbc:mysql://127.0.0.1:3306/simple_demo?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true");
-        vo.setDataBaseUserName("root");
-        vo.setDataBasePassword("123456");
-        vo.setPackageName("cn.iosd.demo");
-        vo.setProjectName("simple-service-generator");
-        vo.setModuleName("generator");
-        vo.setTableNames(Collections.singletonList("demo_article"));
-        List<String> tablePrefix = new ArrayList<>();
-        tablePrefix.add("demo");
-        vo.setTablePrefix(tablePrefix);
-        DatabaseGenService.generate(vo);
-        log.info("文件生成目录：" + new File("target/generator").getAbsolutePath());
+        SpringApplication.run(BaseGeneratorApplication.class, args);
     }
 }
 
---
+++ simple-demo/demo-base-generator/src/test/java/BaseGeneratorTestApplication.java
@@ -0,0 +1,33 @@
+import cn.iosd.base.generator.service.DatabaseGenService;
+import cn.iosd.base.generator.vo.DatabaseGenVo;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ok1996
+ */
+@Slf4j
+public class BaseGeneratorTestApplication {
+    public static void main(String[] args) {
+        DatabaseGenVo vo = new DatabaseGenVo();
+        vo.setAuthorName("ok1996");
+        vo.setDataBaseUrl("jdbc:mysql://127.0.0.1:3306/simple_demo?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8");
+        vo.setDataBaseUserName("root");
+        vo.setDataBasePassword("123456");
+        vo.setPackageName("cn.iosd.demo");
+        vo.setProjectName("simple-service-generator");
+        vo.setModuleName("generator");
+        vo.setTableNames(Collections.singletonList("demo_article"));
+        List<String> tablePrefix = new ArrayList<>();
+        tablePrefix.add("demo");
+        vo.setTablePrefix(tablePrefix);
+        DatabaseGenService.generate(vo);
+        log.info("文件生成目录：" + new File("target/generator").getAbsolutePath());
+    }
+}
+
+
