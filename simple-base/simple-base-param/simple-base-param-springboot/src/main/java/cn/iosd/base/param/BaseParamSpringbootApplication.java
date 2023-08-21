7e1d0421e9159df574d117818296535c392e65e9
ok1996 <ku29@qq.com>
2023-08-21 14:59:37 +0800

    feat:[simple-base-param]增加Springboot启动器

---
+++ simple-base/simple-base-param/simple-base-param-springboot/src/main/java/cn/iosd/base/param/BaseParamSpringbootApplication.java
@@ -0,0 +1,18 @@
+package cn.iosd.base.param;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ok1996
+ */
+@SpringBootApplication
+public class BaseParamSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseParamSpringbootApplication.class, args);
+    }
+}
+
+
