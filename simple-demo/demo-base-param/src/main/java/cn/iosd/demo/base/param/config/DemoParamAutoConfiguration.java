a0c48a2fcd704876a2477a21e2f09ec177b31047
ok1996 <ku29@qq.com>
2023-08-23 16:25:03 +0800

    optimize:[demo-base-param]增加Flyway

---
+++ simple-demo/demo-base-param/src/main/java/cn/iosd/demo/base/param/config/DemoParamAutoConfiguration.java
@@ -0,0 +1,31 @@
+package cn.iosd.demo.base.param.config;
+
+import cn.iosd.starter.datasource.mybatis.MapperLocations;
+import org.flywaydb.core.Flyway;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * 配置Flyway
+ *
+ * @author ok1996
+ */
+@Configuration
+public class DemoParamAutoConfiguration {
+
+    @Bean
+    public Flyway paramFlyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/mysql/demo/param")
+                .baselineOnMigrate(true)
+                .table("demo_param_flyway_schema_history")
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
+
+}
