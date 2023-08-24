5f942251f96a4bbe457eeb7c541f717b45daa3a3
ok1996 <ku29@qq.com>
2023-08-24 16:56:07 +0800

    version: 2023.5.0.0

--- simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/vo/ProjectGenVo.java
+++ simple-base/simple-base-generator/src/main/java/cn/iosd/base/generator/vo/ProjectGenVo.java
@@ -19,7 +19,7 @@ public class ProjectGenVo {
     private String springBootVersion = "3.1.2";
 
     @Schema(description = "依赖模块版本号", hidden = true)
-    private String simpleVersion = "2023.5.0.0-SNAPSHOT";
+    private String simpleVersion = "2023.5.0.0";
 
     @Schema(description = "JAVA版本号", hidden = true)
     private String javaVersion = "17";
