1f342b24b12aa0f52f0dff0f46a00086878e1280
ok1996 <ku29@qq.com>
2023-08-29 16:03:20 +0800

    optimize:[simple-base-param]优化记录历史配置

--- simple-base/simple-base-param/simple-base-param-service/src/main/java/cn/iosd/base/param/service/service/BaseParamServiceImpl.java
+++ simple-base/simple-base-param/simple-base-param-service/src/main/java/cn/iosd/base/param/service/service/BaseParamServiceImpl.java
@@ -5,17 +5,22 @@ import cn.iosd.base.param.api.service.IBaseParamService;
 import cn.iosd.base.param.api.utils.ParamInitUtil;
 import cn.iosd.base.param.api.vo.BaseParamVo;
 import cn.iosd.base.param.api.vo.CodeValue;
+import cn.iosd.base.param.api.vo.CodeValueListHistory;
 import cn.iosd.base.param.service.entity.BaseParamEntity;
 import cn.iosd.base.param.service.mapper.BaseParamMapper;
 import cn.iosd.utils.JsonMapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author ok1996
@@ -61,21 +66,53 @@ public class BaseParamServiceImpl extends ServiceImpl<BaseParamMapper, BaseParam
     /**
      * 请求实体转换Domain
      *
-     * @param baseParamVo
-     * @return
+     * @param baseParamVo 请求实体
+     * @return 转换后的Entity实体
      */
     public BaseParamEntity convertBaseParam(BaseParamVo baseParamVo) {
         BaseParamEntity baseParam = new BaseParamEntity();
         baseParam.setId(baseParamVo.getId());
+
+        String codeValueListHistory = Optional.ofNullable(baseParamVo.getId())
+                .map(baseMapper::selectById)
+                .map(BaseParamEntity::getHistoryCodeValues)
+                .orElse(null);
         try {
             baseParam.setModuleNames(JsonMapper.getObjectMapper().writeValueAsString(baseParamVo.getModuleNames()));
             baseParam.setCodeValues((JsonMapper.getObjectMapper().writeValueAsString(baseParamVo.getCodeValues())));
+            baseParam.setHistoryCodeValues(inputHistoryCodeValues(baseParamVo.getCodeValues(), codeValueListHistory));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         baseParam.setRemark(baseParamVo.getRemark());
         baseParam.setParamKey(baseParamVo.getParamKey());
         return baseParam;
+    }
+
+
+    /**
+     * 将新值添加到历史配置值字符串
+     *
+     * @param codeValueList        新配置值
+     * @param codeValueListHistory 历史配置值字符串
+     * @return 增加了新值的历史配置值字符串
+     * @throws JsonProcessingException JSON处理异常
+     */
+    public String inputHistoryCodeValues(List<CodeValue<?>> codeValueList, String codeValueListHistory) throws JsonProcessingException {
+        List<CodeValueListHistory> resHistoryObj = new ArrayList<>();
+        Integer historySize = 10;
+        if (!StringUtils.isBlank(codeValueListHistory)) {
+            resHistoryObj = JsonMapper.readValue(codeValueListHistory, new TypeReference<List<CodeValueListHistory>>() {
+            });
+        }
+        CodeValueListHistory cvHistoryToAdd = new CodeValueListHistory();
+        cvHistoryToAdd.setTime(System.currentTimeMillis());
+        cvHistoryToAdd.setCodeValueList(codeValueList);
+        resHistoryObj.add(cvHistoryToAdd);
+        if (resHistoryObj.size() > historySize) {
+            resHistoryObj = resHistoryObj.subList(resHistoryObj.size() - historySize, resHistoryObj.size());
+        }
+        return JsonMapper.getObjectMapper().writeValueAsString(resHistoryObj);
     }
 
 }
