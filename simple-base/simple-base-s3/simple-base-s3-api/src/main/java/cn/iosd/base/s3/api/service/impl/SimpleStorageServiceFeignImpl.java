aec87fed9637bcf44081c420d43eb0c0258538c5
ok1996 <ku29@qq.com>
2023-09-04 09:50:08 +0800

    optimize:[demo-base-s3]移除没有使用到的包

--- simple-base/simple-base-s3/simple-base-s3-api/src/main/java/cn/iosd/base/s3/api/service/impl/SimpleStorageServiceFeignImpl.java
+++ simple-base/simple-base-s3/simple-base-s3-api/src/main/java/cn/iosd/base/s3/api/service/impl/SimpleStorageServiceFeignImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 /**
