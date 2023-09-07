50fae5b29eb5e19f8b635dcecfbd52726200833b
ok1996 <ku29@qq.com>
2023-09-07 09:51:45 +0800

    optimize:[simple-starter-datasource]指明基础接口实现

--- simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/base/BaseController.java
+++ simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/base/BaseController.java
@@ -1,68 +1,66 @@
 package cn.iosd.starter.datasource.base;
 
+import cn.iosd.starter.datasource.domain.PageRequest;
+import cn.iosd.starter.datasource.utils.DsConvertUtil;
+import cn.iosd.starter.web.base.CrudOperations;
+import cn.iosd.starter.web.domain.Response;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-import cn.iosd.starter.datasource.domain.PageRequest;
-import cn.iosd.starter.datasource.utils.DsConvertUtil;
-import cn.iosd.starter.web.domain.Response;
-import io.swagger.v3.oas.annotations.Operation;
-
 /**
- * @author ok1996
+ * 单表CURD接口
+ *
  * @param <T> 实体
+ * @author ok1996
  */
 @Component
-public class BaseController<T> {
+public class BaseController<T> implements CrudOperations<T> {
 
     @Autowired
     private IService<T> service;
 
+    @Override
     @Operation(summary = "Api-新增")
-    @PostMapping("/api")
     public Response<Boolean> apiSave(@RequestBody T entity) {
         setValue(entity, "setCreateTime", LocalDateTime.now(), LocalDateTime.class);
         return Response.ok(service.save(entity));
     }
 
+    @Override
     @Operation(summary = "Api-更新-Id")
-    @PutMapping("/api/{id}")
     public Response<Boolean> apiUpdateById(@PathVariable Long id, @RequestBody T entity) {
         setValue(entity, "setId", id, Long.class);
         setValue(entity, "setModifyTime", LocalDateTime.now(), LocalDateTime.class);
         return Response.ok(service.updateById(entity));
     }
 
+    @Override
     @Operation(summary = "Api-删除")
-    @DeleteMapping("/api/{id}")
     public Response<Boolean> apiRemoveById(@PathVariable Long id) {
         return Response.ok(service.removeById(id));
     }
 
+    @Override
     @Operation(summary = "Api-查询-单个")
-    @GetMapping("/api/{id}")
     public Response<T> apiGetById(@PathVariable Long id) {
         return Response.ok(service.getById(id));
     }
 
+    @Override
     @Operation(summary = "Api-查询-列表")
-    @GetMapping("/api/list")
     public Response<List<T>> apiList(@ParameterObject T req) {
         return Response.ok(service.list(Wrappers.lambdaQuery(req)));
     }
