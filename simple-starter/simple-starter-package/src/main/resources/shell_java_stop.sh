f7d93d58c8db17b394318b8d9fd84d352da0ab6e
ok1996 <ku29@qq.com>
2023-10-27 09:47:11 +0800

    optimize:[simple-starter-package]Add a check to see if the process still exists

--- simple-starter/simple-starter-package/src/main/resources/shell_java_stop.sh
+++ simple-starter/simple-starter-package/src/main/resources/shell_java_stop.sh
@@ -11,5 +11,15 @@ if [ -z "$pid" ]; then
 else
   # 杀死进程
   kill -9 "$pid"
-  echo "$service_name 进程 $pid 已被关闭"
+
+  # 循环检测进程是否还存在，最多3次
+  for i in {1..3}; do
+    sleep 1  # 可以适当调整等待时间
+    if ps -p "$pid" > /dev/null; then
+      echo "正在关闭 $service_name 进程 $pid (尝试 $i/3)"
+    else
+      echo "$service_name 进程 $pid 已被关闭"
+      break
+    fi
+  done
 fi
