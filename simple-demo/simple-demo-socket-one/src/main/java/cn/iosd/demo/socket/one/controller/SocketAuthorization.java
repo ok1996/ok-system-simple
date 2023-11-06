d7abbbb2af8c39c1e72e4282fe8f90929737656c
ok1996 <ku29@qq.com>
2023-11-06 11:00:49 +0800

    optimize:[simple-demo-socket-one]Update the netty-socketio version and modify the AuthorizationListener authentication method

--- simple-demo/simple-demo-socket-one/src/main/java/cn/iosd/demo/socket/one/controller/SocketAuthorization.java
+++ simple-demo/simple-demo-socket-one/src/main/java/cn/iosd/demo/socket/one/controller/SocketAuthorization.java
@@ -1,6 +1,7 @@
 package cn.iosd.demo.socket.one.controller;
 
 import com.corundumstudio.socketio.AuthorizationListener;
+import com.corundumstudio.socketio.AuthorizationResult;
 import com.corundumstudio.socketio.HandshakeData;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -17,20 +18,20 @@ import org.springframework.context.annotation.Configuration;
 public class SocketAuthorization implements AuthorizationListener {
 
     /**
-     * 连接Url：http://localhost:12010?room=1003&token=1003
+     * 连接Url：localhost:12010?room=1003&token=1003
      *
-     * @param data
-     * @return
+     * @param handshakeData 请求参数
+     * @return AuthorizationResult
      */
     @Override
-    public boolean isAuthorized(HandshakeData data) {
-        String token = data.getSingleUrlParam("token");
-        String room = data.getSingleUrlParam("room");
+    public AuthorizationResult getAuthorizationResult(HandshakeData handshakeData) {
+        String token = handshakeData.getSingleUrlParam("token");
+        String room = handshakeData.getSingleUrlParam("room");
         log.info("socket认证参数: token={}, room={}", token, room);
         if (StringUtils.isEmpty(token) || StringUtils.isEmpty(room)) {
             log.error("socket认证失败, 参数不符合要求: token={}, room={}", token, room);
-            return false;
+            return AuthorizationResult.FAILED_AUTHORIZATION;
         }
-        return true;
+        return AuthorizationResult.SUCCESSFUL_AUTHORIZATION;
     }
 }
