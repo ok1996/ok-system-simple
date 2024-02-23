e4ed09721e4dc9a40bc1ba2edb43f923dd553721
ok1996 <ku29@qq.com>
2024-02-23 09:34:42 +0800

    optimize:[simple-starter-redisson]Modify cache name constants

--- simple-starter/simple-starter-redisson/src/main/java/cn/iosd/starter/redisson/domain/CacheName.java
+++ simple-starter/simple-starter-redisson/src/main/java/cn/iosd/starter/redisson/domain/CacheName.java
@@ -2,15 +2,15 @@ package cn.iosd.starter.redisson.domain;
 
 public class CacheName {
     /**
-     * 缓存时间10分钟
+     * 缓存时间10分钟 Simple Redisson Cache: Expires Ten Minutes
      */
-    public static final String EXPIRES_AFTER_TEN_MINUTES = "cacheExpiresAfterTenMinutes";
+    public static final String EXPIRES_AFTER_TEN_MINUTES = "SimpleRCETM";
     /**
-     * 缓存时间60分钟
+     * 缓存时间60分钟 Simple Redisson Cache: Expires One Hour
      */
-    public static final String EXPIRES_AFTER_ONE_HOUR = "cacheExpiresAfterOneHour";
+    public static final String EXPIRES_AFTER_ONE_HOUR = "SimpleRCEOH";
     /**
-     * 缓存不失效
+     * 缓存不失效 Simple Redisson Cache: Expires Never
      */
-    public static final String NEVER_EXPIRES = "cacheNeverExpires";
+    public static final String NEVER_EXPIRES = "SimpleRCEN";
 }
