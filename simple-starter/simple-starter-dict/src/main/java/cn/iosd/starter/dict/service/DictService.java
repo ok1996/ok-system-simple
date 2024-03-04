55d83286719240d85918ddf038aca7054d83f37b
ok1996 <ku29@qq.com>
2024-03-04 15:37:35 +0800

    optimize:[simple-starter-dict]Modify comments explanation

--- simple-starter/simple-starter-dict/src/main/java/cn/iosd/starter/dict/service/DictService.java
+++ simple-starter/simple-starter-dict/src/main/java/cn/iosd/starter/dict/service/DictService.java
@@ -9,10 +9,10 @@ import java.util.List;
  */
 public interface DictService {
     /**
-     * 获取指定类型的字典项列表
+     * 根据参数获取对应字典列表
      *
-     * @param dictionaryParams 接口实现类所需的参数类型
-     * @return 参数类型对应字典列表
+     * @param dictionaryParams 获取对应字典列表所需的参数
+     * @return 字典列表
      */
     List<DictItem> getDictItemList(String dictionaryParams);
 }
