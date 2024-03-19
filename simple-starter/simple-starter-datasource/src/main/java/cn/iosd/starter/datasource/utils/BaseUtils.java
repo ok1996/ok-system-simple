13eb72bd8b758eeeb051c81dbe7b16ebcdea0c89
ok1996 <ku29@qq.com>
2024-03-19 10:27:29 +0800

    optimize:[simple-starter-datasource]Extract utility methods

---
+++ simple-starter/simple-starter-datasource/src/main/java/cn/iosd/starter/datasource/utils/BaseUtils.java
@@ -0,0 +1,26 @@
+package cn.iosd.starter.datasource.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class BaseUtils {
+
+    /**
+     * 通过反射调用实体类对象中的指定方法，设置指定的值。
+     *
+     * @param entity     需要设置值的实体类对象
+     * @param methodName 需要调用的方法名
+     * @param value      需要设置的值
+     * @param valueType  value参数的类型
+     * @param <T>        实体类对象的类型
+     * @param <V>        value参数的类型
+     */
+    public static <T, V> void setValue(T entity, String methodName, V value, Class<V> valueType) {
+        try {
+            Method method = entity.getClass().getMethod(methodName, valueType);
+            method.invoke(entity, value);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            // T实体不存在方法，不做处理
+        }
+    }
+}
