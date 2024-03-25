09ad75534eb7e95df190df13624922e04ffd1e98
ok1996 <ku29@qq.com>
2024-03-25 10:00:20 +0800

    update: [Spring Boot] 3.2.3 -> 3.2.4

--- simple-base/simple-base-generator/simple-base-generator-api/src/main/java/cn/iosd/base/generator/api/vo/ProjectGenVo.java
+++ simple-base/simple-base-generator/simple-base-generator-api/src/main/java/cn/iosd/base/generator/api/vo/ProjectGenVo.java
@@ -19,7 +19,7 @@ public class ProjectGenVo {
     private String projectPort;
 
     @Schema(description = "SpringBoot版本号", hidden = true)
-    private String springBootVersion = "3.2.3";
+    private String springBootVersion = "3.2.4";
 
     @Schema(description = "项目版本号", hidden = true)
     private String projectVersion = "2024.1.0.0-SNAPSHOT";
